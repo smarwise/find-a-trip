d8a97c48d1d0374721786720e6756c8741f7a514
smarwise <smarwise@student.wethinkcode.co.za>
2021-07-13 08:38:29 +0200

    added select start and destination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+// import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
@@ -12,6 +14,13 @@ import {MatListModule} from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';  
 import { AppComponent } from './app.component';
 import { TripComponent } from './trip/trip.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -27,7 +36,14 @@ import { TripComponent } from './trip/trip.component';
     MatGridListModule,
     MatIconModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatSelectModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/trip/trip.component.ts
+++ src/app/trip/trip.component.ts
@@ -42,10 +42,12 @@ export class Journey{
 export class TripComponent implements OnInit {
 
   testData = [
-   {"type": "bus", "duration": "10", "fare": "$10"}, 
-   {"type": "rail", "duration": "4", "fare": "$50"}, 
-   {"type": "bus", "duration": "25", "fare": "$20"}, 
+   {"type": "Bus", "duration": "10", "fare": "$10"}, 
+   {"type": "Rail", "duration": "4", "fare": "$50"}, 
+   {"type": "Bus", "duration": "25", "fare": "$20"}, 
   ]
+
+  // menu = ["no", "star"];
 
   title = 'Trips';
   trips: Trip[] = [];
@@ -53,8 +55,22 @@ export class TripComponent implements OnInit {
   journeys: Journey[] = [];
   accessToken: string = "";
   columnsToDisplay = ["type", "duration", "fare"];
+  selected2 = "Johannesburg";
+  selected1 = "Randburg";
+
+  stops = [{"key": "Johannesburg", "longitude":28.0473, "latitude": 26.2041},
+  {"key": "Centurion", "longitude":28.0889, "latitude": 25.8640},
+  {"key": "Kempton Park", "longitude":28.2337, "latitude": 26.1084},
+  {"key": "Fourways", "longitude":28.056702, "latitude": -26.107567},
+  {"key": "Rosebank", "longitude":18.4742, "latitude": -33.9556},
+  {"key": "Randburg", "longitude":27.9952, "latitude": 26.1438},
+  {"key": "Sandton", "longitude":28.0567, "latitude": 26.1076},
+  {"key": "Germiston", "longitude":28.1708, "latitude": 26.2259},
+  {"key": "Eastgate", "longitude":28.659, "latitude": 26.1047}]
+
   headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
-  token_headers = new HttpHeaders();
+  token_headers = new HttpHeaders()
+;
   params = new HttpParams();
   body = {
     "geometry": {
@@ -82,15 +98,16 @@ export class TripComponent implements OnInit {
   }
 
   getAccessToken(){
-    this.httpClient.post<any>('https://identity.whereismytransport.com/connect/token', "client_id=7fe6814d-26f5-4f5b-aa5c-e180f46757d4&client_secret=c0S4%2Ff135fOOLhiRwMQEZhprh9shVxEniBUvVphtlAc%3D&grant_type=client_credentials&scope=transportapi%3Aall", 
-    {headers: this.headers}).subscribe(
-      response => {
-        this.accessToken = response.access_token;
-        this.getTrips();
-      }
-    , (err) => {
-      console.log(err);
-    });
+    console.log(this.selected1+this.selected2);
+    // this.httpClient.post<any>('https://identity.whereismytransport.com/connect/token', "client_id=7fe6814d-26f5-4f5b-aa5c-e180f46757d4&client_secret=c0S4%2Ff135fOOLhiRwMQEZhprh9shVxEniBUvVphtlAc%3D&grant_type=client_credentials&scope=transportapi%3Aall", 
+    // {headers: this.headers}).subscribe(
+    //   response => {
+    //     this.accessToken = response.access_token;
+    //     this.getTrips();
+    //   }
+    // , (err) => {
+    //   console.log(err);
+    // });
   }
 
 getTrips(){
@@ -122,6 +139,13 @@ getJourneys(){
       });
   });
   console.log(this.journeys);
+}
+
+modeIcon(mode: string): string {
+  if(mode == "Walking") {return 'directions_walk'} 
+  if(mode == "Bus") {return 'directions_bus'} 
+  if(mode == "Rail") {return 'subway'}
+  return ("rowing");
 }
 
 }
